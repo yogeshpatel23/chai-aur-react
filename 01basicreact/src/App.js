6d6f74ac18e4cf4016b53387465891a27444c51f
Yogesh Patel <yogesh.d.p.patel@gmail.com>
2023-12-05 14:14:42 +0530

    Larn how react work

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,5 +1,13 @@
+import Chai from "./chai";
+
 function App() {
-  return <h1>Chai aur React | Yogesh </h1>;
+  return (
+    <>
+      <h1>Chai aur React | Yogesh </h1>
+      <Chai />
+      <p>Componets name must be start whith Capital latter</p>
+    </>
+  );
 }
 
 export default App;
---
+++ 01basicreact/src/chai.js
@@ -0,0 +1,5 @@
+function chai() {
+  return <h2>chai in react</h2>;
+}
+
+export default chai;
