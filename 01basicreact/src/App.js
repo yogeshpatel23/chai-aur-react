3e8bbb46c39fdaa9f8f6b1bd0217b0dbaceb5ad3
Yogesh Patel <yogesh.d.p.patel@gmail.com>
2023-12-05 12:37:52 +0530

    React setup with create-react-app and vite

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Chai aur React | Yogesh </h1>;
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h>Chai aur react with vite | Yogesh</h>;
+}
+
+export default App;
