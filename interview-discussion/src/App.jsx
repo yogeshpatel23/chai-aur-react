f5bef2a89e1eb43a67196929c3c35a4ecd04aa97
Yogesh Patel <yogesh.d.p.patel@gmail.com>
2023-12-08 19:02:52 +0530

    More intervier

--- interview-discussion/src/App.jsx
+++ interview-discussion/src/App.jsx
@@ -4,20 +4,25 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  console.log("app resnder", Math.random());
   const [value, setValue] = useState(1);
   // const [multipliedValue, setMultipliedValue] = useState(1);
-  let multipliedValue = value * 5;
+  // let multipliedValue = value * 5;
 
-  const multiplybyfive = () => {
-    // setMultipliedValue(() => value * 5);
-    setValue(() => value + 1);
+  // const multiplybyfive = () => {
+  //   // setMultipliedValue(() => value * 5);
+  //   setValue(() => value + 1);
+  // };
+
+  const clickMe = () => {
+    console.log("first");
+    setValue(3);
   };
-
   return (
     <>
       <h1>Main Value : {value}</h1>
-      <button onClick={multiplybyfive}>Click to multiply by: 5</button>
-      <h2>Multiplied value: {multipliedValue} </h2>
+      <button onClick={clickMe}>Click to multiply by: 5</button>
+      {/* <h2>Multiplied value: {multipliedValue} </h2> */}
     </>
   );
 }
